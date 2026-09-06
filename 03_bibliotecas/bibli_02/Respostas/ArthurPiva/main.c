eb3a0609bfe1069764e208776bc319f156ae326b
arthurpiva12 <271388542+arthurpiva12@users.noreply.github.com>
2026-09-06 15:34:27 -0300

    exercicio biblio 2completo

--- 03_bibliotecas/bibli_02/Respostas/ArthurPiva/main.c
+++ 03_bibliotecas/bibli_02/Respostas/ArthurPiva/main.c
@@ -15,34 +15,87 @@ void ImprimeOpcoes(){
 
 int main(){
     int rows1, cols1, rows2, cols2;
-    int matrix1[rows1][cols1], matrix2[rows2][cols2];
 
-    matrix_read(rows1, cols1, matrix1[rows1][cols1]);
-    matrix_read(rows2, cols2, matrix2[rows2][cols2]);
+    scanf("%d %d", &rows1, &cols1);
+    int matrix1[rows1][cols1];
+    matrix_read(rows1, cols1, matrix1);
+
+    scanf("%d %d", &rows2, &cols2);
+    int matrix2[rows2][cols2];
+    matrix_read(rows2, cols2, matrix2);
 
     int opcao = 0;
 
-    while(opcao != 6){
+    while(1){
         int possibilidade = 2;
-        scanf("%d", &opcao);
         ImprimeOpcoes();
+        scanf("%d", &opcao);
 
-        if(opcao == 1){
+        if(opcao == 6){
+            break;
+        }
+
+        else if(opcao == 1){
             possibilidade = possible_matrix_sum(rows1, cols1, rows2, cols2);
             if(possibilidade == 1){
                 int result[rows1][cols1];
-                matrix_add(rows1, cols1, matrix1[rows1][cols1], rows2, cols2, matrix2[rows2][cols2], result[rows1][cols1]);
-                matrix_print(rows1, cols1, result[rows1][cols1]);
+                matrix_add(rows1, cols1, matrix1, rows2, cols2, matrix2, result);
+                matrix_print(rows1, cols1, result);
+            }
+            else{
+                printf("Erro: as dimensoes da matriz nao correspondem\n\n");
             }
         }
 
-        if(opcao == 2){
+        else if(opcao == 2){
             possibilidade = possible_matrix_sub(rows1, cols1, rows2, cols2);
             if(possibilidade == 1){
                 int result[rows1][cols1];
-                matrix_sub(rows1, cols1, matrix1[rows1][cols1], rows2, cols2, matrix2[rows2][cols2], result[rows1][cols1]);
+                matrix_sub(rows1, cols1, matrix1, rows2, cols2, matrix2, result);
+                matrix_print(rows1, cols1, result);
+            }
+            else{
+                printf("Erro: as dimensoes da matriz nao correspondem\n\n");
             }
         }
+
+        else if(opcao == 3){
+            possibilidade = possible_matrix_multiply(cols1, rows2);
+            if(possibilidade == 1){
+                int result[rows1][cols2];
+                matrix_multiply(rows1, cols1, matrix1, rows2, cols2, matrix2, result);
+                matrix_print(rows1, cols2, result);
+            }
+            else{
+                printf("Erro: o numero de colunas da primeira matriz eh diferente do numero de linhas da segunda matriz\n\n");
+            }
+        }
+
+        else if(opcao == 4){
+            int scalar, matrizDesejada;
+            scanf("%d %d", &scalar, &matrizDesejada);
+
+            if(matrizDesejada == 1){
+                scalar_multiply(rows1, cols1, matrix1, scalar);
+                matrix_print(rows1, cols1, matrix1);
+            }
+            else{
+                scalar_multiply(rows2, cols2, matrix2, scalar);
+                matrix_print(rows2, cols2, matrix2);
+            }
+        }
+
+        else if(opcao == 5){
+            int result[rows1][cols1];
+            int resultado[rows2][cols2];
+
+            transpose_matrix(rows1, cols1, matrix1, result);
+            transpose_matrix(rows2, cols2, matrix2, resultado);
+
+            matrix_print(cols1, rows1, result);
+            matrix_print(cols2, rows2, resultado);
+        }
+
     }
 
     return 0;
--- 03_bibliotecas/bibli_02/Respostas/ArthurPiva/matrix_utils.c
+++ 03_bibliotecas/bibli_02/Respostas/ArthurPiva/matrix_utils.c
@@ -3,21 +3,13 @@
 
 void matrix_read(int rows, int cols, int matrix[rows][cols]){
     int i=0, j=0;
-    scanf("%d %d", &rows, &cols);
 
     for(i=0; i<rows; i++){
         for(j=0; j<cols; j++){
-            scanf("%d", matrix[i][j]);
+            scanf("%d", &matrix[i][j]);
         }
     }
 
-    for(i=0; i<rows; i++){
-        for(j=0; j<cols; j++){
-            scanf("%d", matrix[i][j]);
-        }
-    }
-
-    return matrix[rows][cols];
 }
 
 void matrix_print(int rows, int cols, int matrix[rows][cols]){
@@ -30,38 +22,28 @@ void matrix_print(int rows, int cols, int matrix[rows][cols]){
                 printf("%d ", matrix[i][j]);
             }
             else{
-                printf("%d ", matrix[i][j]);
+                printf("%d|", matrix[i][j]);
             }
         }
         printf("\n");
     }
+
+    printf("\n");
 }
 
 int possible_matrix_sum(int rows1, int cols1, int rows2, int cols2){
-    if(rows1 == rows2){
-        if(cols2 == cols2){
-            return 1;
-        }
-        else{
-            return 0;
-        }
+    if(rows1 == rows2 && cols1 == cols2){
+        return 1;
     }
-
     else{
         return 0;
     }
 }
 
 int possible_matrix_sub(int rows1, int cols1, int rows2, int cols2){
-    if(rows1 == rows2){
-        if(cols2 == cols2){
-            return 1;
-        }
-        else{
-            return 0;
-        }
+    if(rows1 == rows2 && cols1 == cols2){
+        return 1;
     }
-
     else{
         return 0;
     }
@@ -91,20 +73,43 @@ void matrix_sub(int rows1, int cols1, int matrix1[rows1][cols1], int rows2, int 
 
     for(i=0; i<rows1; i++){
         for(j=0; j<cols1; j++){
-            result[i][j] = matrix1[i][j] + matrix2[i][j];
+            result[i][j] = matrix1[i][j] - matrix2[i][j];
         }
     }
 }
 
 void matrix_multiply(int rows1, int cols1, int matrix1[rows1][cols1], int rows2, int cols2, int matrix2[rows2][cols2], int result[rows1][cols2]){
+    int i=0, j=0, k=0;
+
+    for(i=0; i<rows1; i++){
+        for(j=0; j<cols2; j++){
+            result[i][j] = 0;
+
+            for(k=0; k<cols1; k++){
+                result[i][j] = matrix1[i][k] * matrix2[k][j] + result[i][j];
+            }
+        }
+    }
 
 }
 
 void transpose_matrix(int rows, int cols, int matrix[rows][cols], int result[cols][rows]){
+    int i=0, j=0;
 
+    for(i=0; i<cols; i++){
+        for(j=0; j<rows; j++){
+            result[i][j] = matrix[j][i];
+        }
+    }
 }
 
 void scalar_multiply(int rows, int cols, int matrix[rows][cols], int scalar){
+    int i=0, j=0;
 
+    for(i=0; i<rows; i++){
+        for(j=0; j<cols; j++){
+            matrix[i][j] = matrix[i][j] * scalar;
+        }
+    }
 }
 
